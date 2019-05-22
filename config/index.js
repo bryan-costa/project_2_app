1e0339875c41a153b959fce53d9c673f398d066b
The_Costa <bryan.costa@roadrunner.com>
2019-05-21 19:49:30 -0700

    Easier to tell if you're production/local DB

--- config/index.js
+++ config/index.js
@@ -3,7 +3,9 @@ let sequelize
 
 if (process.env.NODE_ENV === 'PRODUCTION'){
     sequelize = new Sequelize(process.env.JAWSDB_URL)
+    console.log("PRODUCTION DB!!!")
 } else {
     sequelize = new Sequelize(process.env.LOCAL_URL)
+    console.log("local DB")
 }
 module.exports = sequelize
