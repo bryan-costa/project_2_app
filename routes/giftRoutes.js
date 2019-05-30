f086848d7bc78db425c1e68b319e21ac1576b260
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-05-29 19:13:03 -0700

    all basic routes working

--- routes/giftRoutes.js
+++ routes/giftRoutes.js
@@ -2,18 +2,21 @@ const { Gift } = require('../models')
 
 module.exports = app => {
     // GET all gifts
+    // this is working!
     app.get('/gifts', (req, res) => {
         Gift.findAll()
             .then(user => res.json(user))
             .catch(e => console.log(e))
     })
     // GET one gift
+    // this is working if I use findByPk instead of findOne
     app.get('/gifts/:id', (req, res) => {
-        Gift.findOne(req.params.id)
+        Gift.findByPk(req.params.id)
             .then(user => res.json(user))
             .catch(e => console.log(e))
     })
     // POST a gift
+    // this is working!
     app.post('/gifts', (req, res) => {
         Gift.create(req.body)
             .then(user => res.sendStatus(200))
@@ -21,9 +24,18 @@ module.exports = app => {
     })
 
     //PUT a gift
-    app.put('gifts/:id', (req, res) => {
+    // this is working!
+    app.put('/gifts/:id', (req, res) => {
         Gift.update(req.body, { where: { id: req.params.id } })
         .then(_ => res.sendStatus(200))
+        .catch(e => console.log(e))
+    })
+
+    //DELETE a gift
+    // this is working!
+    app.delete('/gifts/:id', (req, res) => {
+        Gift.destroy( { where: { id: req.params.id } })
+        .then(user => res.sendStatus(200))
         .catch(e => console.log(e))
     })
 }
--- routes/purchaseRoutes.js
+++ routes/purchaseRoutes.js
@@ -2,13 +2,23 @@ const { Purchase } = require('../models')
 
 module.exports = app => {
     // GET all Purchases
+    // this is working!
     app.get('/purchases', (req, res) => {
         Purchase.findAll()
             .then(purchase => res.json(purchase))
             .catch(e => console.log(e))
     })
 
+    // GET one purchase
+    // this works by changing findOne to findByPk
+    app.get('/purchases/:id', (req, res) => {
+        Purchase.findByPk(req.params.id)
+        .then(purchase => res.json(purchase))
+        .catch(e => console.log(e))
+        })
+
     // POST a Purchase
+    // this is working!
     app.post('/purchases', (req, res) => {
         Purchase.create(req.body)
             .then(purchase => res.sendStatus(200))
@@ -16,10 +26,17 @@ module.exports = app => {
     })
 
     //PUT a purchase
-    app.put('purchases/:id', (req, res) => {
+    // this is working!
+    app.put('/purchases/:id', (req, res) => {
         Purchase.update(req.body, { where: { id: req.params.id } })
         .then(_ => res.sendStatus(200))
         .catch(e => console.log(e))
     })
-
+    //DELETE a purchase
+    // this is working!
+    app.delete('/purchases/:id', (req, res) => {
+        Purchase.destroy( { where: { id: req.params.id } })
+        .then(user => res.sendStatus(200))
+        .catch(e => console.log(e))
+    })
 }
