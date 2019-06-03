c153d795d5ccd3b128dee7db3bf271a32a1ffdd5
Winsam Roderos <Winsamhehe@Winsams-MacBook-Pro.local>
2019-06-03 00:53:10 -0700

    components

--- src/components/ProfileTop/ProfileTop.js
+++ src/components/ProfileTop/ProfileTop.js
@@ -1,36 +1,70 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IronMan1 from '../../assets/images/IronMan1.png'
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-    display: 'block',
-    position:'right',
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: theme.spacing(0, 3),
   },
+  paper: {
+    maxWidth: 400,
+    margin: `${theme.spacing(1)}px auto`,
+    padding: theme.spacing(2),
+    
+  },
+  button: {
+    width: 200,
+    margin: 5,
+  },
+  buttonbox: {
+    paddingLeft: 5,
+  },
+  bigAvatar:{
+    width: 70,
+    height: 70,
+    paddingTop: 10,
+  },
+});
 
 
 
-}));
-
-function OutlinedButtons() {
-  const classes = useStyles();
+function AutoGridNoWrap(props) {
+  const { classes } = props;
 
   return (
-    <div>
-        <React.Fragment>
-            <Button variant="outlined" className={classes.button}>
-            Wishlist
-            </Button>
-            <Button variant="outlined" className={classes.button}>
-            Friends
-            </Button>
-      </React.Fragment>
-     
-      </div>
+    <div className={classes.root}>
+
+      <Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+          <Avatar alt="Remy Sharp" src={IronMan1} className={classes.bigAvatar} /> 
+          </Grid>
+          <Grid item xs>
+            <div className={classes.buttonbox}>
+          <Button className={classes.button} variant="outlined" className={classes.button}>
+           Wishlist
+          </Button > <br></br>
+          <Button className={classes.button} variant="outlined" className={classes.button}>
+          Friends
+          </Button>
+          </div>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
   );
 }
 
-export default OutlinedButtons;
+AutoGridNoWrap.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
+export default withStyles(styles)(AutoGridNoWrap);
